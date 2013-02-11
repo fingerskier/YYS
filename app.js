fccdc03830cd877b5d75d48ee45f3f392d77e6d8
Matt Currier <fingerskier@gmail.com>
2013-02-10 20:52:36 -0700

    Socket.io
    Still playing with web-sockets, trying to work towards a
    framework/overlay...loving it.

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-var express = require('express');
-var redis = require('redis');
-var routes = require('./routes');
-var socket = require('./routes/socket.js');
+var express = require('express')
+, MemoryStore = express.session.MemoryStore
+, redis = require('redis')
+, pub    = redis.createClient()
+, sub    = redis.createClient()
+, client = redis.createClient()
+, routes = require('./routes')
+, socket = require('./routes/socket.js')
 
-var app = module.exports = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+, app = module.exports = express()
+, server = require('http').createServer(app)
+, io = require('socket.io').listen(server)
+, parseCookie = require('connect').utils.parseCookie
+, sessionStore = new MemoryStore()
+, Session = require('connect').middleware.session.Session;
 
 app.configure(function() {
+  app.use(express.cookieParser());
+  app.use(express.session({store: sessionStore
+    , secret: 'secret'
+    , key: 'express.sid'
+  }));
+  app.use(function (req, res) {
+      res.end('<h2>Hello, your session id is ' + req.sessionID + '</h2>');
+  });
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
@@ -19,7 +34,6 @@ app.configure(function() {
 app.configure('development', function() {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
-
 app.configure('production', function() {
   app.use(express.errorHandler());
 });
@@ -28,6 +42,39 @@ app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
 app.get('*', routes.index);
 
+io.configure(function (){
+  io.set('authorization', function (handshakeData, callback) {
+    // findDatabyip is an async example function
+    findDatabyIP(handshakeData.address.address, function (err, data) {
+      if (err) return callback(err);
+
+      if (data.authorized) {
+        handshakeData.foo = 'bar';
+        for(var prop in data) handshakeData[prop] = data[prop];
+        callback(null, true);
+      } else {
+        callback(null, false);
+      }
+    })
+  });
+});
+io.set('store', sessionStore);
+io.set('authorization', function (data, accept) {
+  if (data.headers.cookie) {
+    data.cookie = parseCookie(data.headers.cookie);
+    data.sessionID = data.cookie['express.sid'];
+    sessionStore.get(data.sessionID, function (err, session) {
+      if (err || !session) {
+        accept('Error', false);
+      } else {
+        data.session = session;
+        accept(null, true);
+      }
+    });
+  } else {
+   return accept('No cookie transmitted.', false);
+  }
+});
 io.sockets.on('connection', socket);
 
 server.listen(3000, function() {
---
+++ routes/api/account.js
@@ -0,0 +1,7 @@
+module.exports = function() {
+  return {
+    get: function() {},
+    login: function() {},
+    logout: function() {}
+  };
+};
